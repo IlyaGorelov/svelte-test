ccdc321de3f7a1f94f08d27a168032805c6565d7
Ilya <ilgorelov5@gmail.com>
2024-03-11 10:05:34 +0400

    add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,35 @@
+<script>
+    const nav = [
+        {
+            title: "Обо мне",
+            href: "/about",
+        },
+        {
+            title: "Блог",
+            href: "/blog",
+        },
+        {
+            title: "Портфолио",
+            href: "/project",
+        },
+        {
+            title: "Контакты",
+            href: "/contact",
+        },
+        {
+            title: "temp",
+            href: "/temp",
+        },
+    ];
+</script>
+
 <nav>
     <div class="nav-container">
         <a href="/" class="nav-logo" title="Back To Homepage">Илья Горелов</a>
         <div class="nav-links">
-            <a href="/about" class="link">Обо мне</a>
-            <a href="/blog" class="link">Блог</a>
-            <a href="/projects" class="link">Портфолио</a>
-            <a href="/contact" class="link">Контакты</a>
+            {#each nav as link}
+                <a href={link.href} class="link">{link.title}</a>
+            {/each}
         </div>
     </div>
 </nav>
