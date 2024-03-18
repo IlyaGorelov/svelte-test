f30899a953c19b298605ce61e71620e317667c6f
Ilya <ilgorelov5@gmail.com>
2024-03-18 09:43:35 +0400

    add blog

---
+++ src/routes/blog/posts/1/+page.svelte
@@ -0,0 +1,48 @@
+<h1>My First Blog Post</h1>
+<p class="lorem200">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat,
+    assumenda. Esse quas hic culpa libero voluptas eveniet reiciendis? Mollitia
+    earum nihil soluta quas recusandae excepturi quos molestiae cum? Sint non ab
+    vero reiciendis tenetur aliquid architecto dolorem, amet magni, ducimus odio
+    fugit! Iste dolore nulla velit voluptates necessitatibus perferendis maiores
+    nemo ea impedit explicabo nihil laboriosam tenetur recusandae enim nostrum
+    odio, nisi eos veritatis eaque corporis libero molestias. Sunt quisquam
+    beatae minima nam. Facere porro delectus ullam tempora sed deserunt aut
+    nihil totam, dolores nesciunt aliquam esse distinctio magni, velit error
+    voluptatibus voluptates quaerat nulla animi beatae. Qui impedit minima
+    voluptate quae nulla! Illum error repudiandae repellendus corporis esse eum
+    animi nisi, ad eius dicta inventore nostrum beatae unde accusantium autem
+    placeat! Praesentium eius consequatur, voluptatem architecto molestias earum
+    quasi blanditiis atque. Temporibus voluptatum provident eligendi,
+    reprehenderit recusandae molestiae veritatis maxime distinctio, assumenda
+    cum reiciendis aspernatur odit, beatae porro magni nostrum aut. Mollitia
+    consequuntur ut nesciunt quasi. Impedit in expedita laudantium excepturi
+    fugiat voluptatibus, consequatur corporis ab amet, id hic? Sunt asperiores
+    eos adipisci, neque voluptatum enim, minima voluptatem quae cum pariatur
+    explicabo accusantium quam nobis illo exercitationem ea nemo consequatur
+    provident, sint aspernatur cupiditate eius autem libero. Impedit, non.
+</p>
+
+<p class="lorem200">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat,
+    assumenda. Esse quas hic culpa libero voluptas eveniet reiciendis? Mollitia
+    earum nihil soluta quas recusandae excepturi quos molestiae cum? Sint non ab
+    vero reiciendis tenetur aliquid architecto dolorem, amet magni, ducimus odio
+    fugit! Iste dolore nulla velit voluptates necessitatibus perferendis maiores
+    nemo ea impedit explicabo nihil laboriosam tenetur recusandae enim nostrum
+    odio, nisi eos veritatis eaque corporis libero molestias. Sunt quisquam
+    beatae minima nam. Facere porro delectus ullam tempora sed deserunt aut
+    nihil totam, dolores nesciunt aliquam esse distinctio magni, velit error
+    voluptatibus voluptates quaerat nulla animi beatae. Qui impedit minima
+    voluptate quae nulla! Illum error repudiandae repellendus corporis esse eum
+    animi nisi, ad eius dicta inventore nostrum beatae unde accusantium autem
+    placeat! Praesentium eius consequatur, voluptatem architecto molestias earum
+    quasi blanditiis atque. Temporibus voluptatum provident eligendi,
+    reprehenderit recusandae molestiae veritatis maxime distinctio, assumenda
+    cum reiciendis aspernatur odit, beatae porro magni nostrum aut. Mollitia
+    consequuntur ut nesciunt quasi. Impedit in expedita laudantium excepturi
+    fugiat voluptatibus, consequatur corporis ab amet, id hic? Sunt asperiores
+    eos adipisci, neque voluptatum enim, minima voluptatem quae cum pariatur
+    explicabo accusantium quam nobis illo exercitationem ea nemo consequatur
+    provident, sint aspernatur cupiditate eius autem libero. Impedit, non.
+</p>
